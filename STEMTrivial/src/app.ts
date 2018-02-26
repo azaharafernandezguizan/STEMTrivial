f4af9ec3c53f0c6cecc34db6d923fc1c7e996604
azaharafernandezguizan <azaharafernandezguizan@gmail.com>
2018-02-26 17:22:24 +0100

    added TODO and modifications in calculating results

--- STEMTrivial/src/app.ts
+++ STEMTrivial/src/app.ts
@@ -14,7 +14,8 @@ export class App {
               { id: 1, text: 'Marie Curie' },
               { id: 2, text: 'Rita Levi Montalcini' }
             ],
-            selectedAnswer : null
+            selectedAnswer : null,
+            correctAnswerId : 0
           },
           {category: "Technology", 
             text: "¿Quién es la CEO de Facebook Iberia en 2018?",
@@ -23,7 +24,8 @@ export class App {
               { id: 1, text: 'Irene Cano' },
               { id: 2, text: 'Arancha Torres' }
             ],
-            selectedAnswer : null
+            selectedAnswer : null,
+            correctAnswerId : 1
           },
           {category: "Maths", 
             text: "¿Quien fue la primera mujer en ocupar una cátedra de matemáticas?",
@@ -32,7 +34,8 @@ export class App {
               { id: 1, text: 'Hipatía de Alejandría' },
               { id: 2, text: 'Sophie Germain' }
             ],
-            selectedAnswer : null
+            selectedAnswer : null,
+            correctAnswerId : 0
           },
           {category: "Engineering", 
           text: "¿Quien fue la primera mujer diplomada como Ingeniera en América del Sur?",
@@ -41,7 +44,8 @@ export class App {
             { id: 1, text: 'Pino Pliego' },
             { id: 2, text: 'Elisa Bachofen' }
           ],
-          selectedAnswer : null
+          selectedAnswer : null,
+          correctAnswerId : 2
         }
         ];
   currentQuestion = null;
@@ -66,11 +70,15 @@ export class App {
   
   nextQuestion(){
     this.indexCurrentQuestion ++;
-    //TODO: comprobar si acertó y sumar aciertos por categoría result.points++
-    debugger;
+
+    if(this.currentQuestion.selectedAnswer.id == this.currentQuestion.correctAnswerId){
+      this.result.points ++;
+    }
+
     if(this.questions.length > this.indexCurrentQuestion){
       this.currentQuestion = this.questions[this.indexCurrentQuestion];
-    }else
+    }
+    else
     {
       this.changeVisibility("Result");
       this.fillResultText();
@@ -98,15 +106,16 @@ export class App {
   }
 
     fillResultText(){
-      //TODO cambiar los points para los resultados en variable y cambiar rangos
-      if(this.result.points > 3){
+      var questionsCount = this.questions.length;
+      debugger;
+      if(this.result.points >= (questionsCount * 0.9)){
         this.result.category = "Gold";
         this.result.explanation = "Has acertado "+this.result.points+ ", medalla de oro, enhorabuena!";
-      } else if(this.result.points >2){
+      } else if(this.result.points >= (questionsCount * 0.7)){
         this.result.category = "Silver";
         this.result.explanation = "Has acertado "+this.result.points+ ", medalla de plata!";
-      } else if(this.result.points >1){
-        this.result.category = "Bronze";
+      } else if(this.result.points >= (questionsCount * 0.5)){
+        this.result.category = "Bronce";
         this.result.explanation = "Has acertado "+this.result.points+ ", medalla de bronce!"
       }else{
         this.result.category = "No ha habido suerte";
